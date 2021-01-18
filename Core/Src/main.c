b55d0bbdf18c9a3ba2f6562355786d2a1ec9bc20
hgbvx <hgbvx33@gmail.com>
2021-01-18 18:59:33 +0100

    Wygenerowany nowy kod

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -19,6 +19,9 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "adc.h"
+#include "dac.h"
+#include "dma.h"
 #include "i2c.h"
 #include "spi.h"
 #include "tim.h"
@@ -119,12 +122,17 @@ double pres;
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+  MX_DMA_Init();
   MX_USART3_UART_Init();
   MX_TIM2_Init();
   MX_TIM3_Init();
   MX_TIM4_Init();
   MX_I2C1_Init();
   MX_SPI4_Init();
+  MX_ADC1_Init();
+  MX_DAC_Init();
+  MX_TIM6_Init();
+  MX_TIM7_Init();
   /* USER CODE BEGIN 2 */
 
   HAL_UART_Receive_IT   (&huart3  , (uint8_t*) rx_buffer  , 3);
