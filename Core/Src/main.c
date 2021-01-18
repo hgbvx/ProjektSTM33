262abe75308f839265a22208cd7a4941ad3098eb
hgbvx <hgbvx33@gmail.com>
2021-01-18 18:56:31 +0100

    Podstawka

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -6,7 +6,7 @@
   ******************************************************************************
   * @attention
   *
-  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
+  * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
   * All rights reserved.</center></h2>
   *
   * This software component is licensed by ST under BSD 3-Clause license,
@@ -19,9 +19,6 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-#include "adc.h"
-#include "dac.h"
-#include "dma.h"
 #include "i2c.h"
 #include "spi.h"
 #include "tim.h"
@@ -30,12 +27,16 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "bmp280.h"
+#include "bmp280_defs.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN PTD */
+#define BMP280_SPI (&hspi4)
 
+#define BMP280_CS1 1
+#define BMP280_CS2 2
 /* USER CODE END PTD */
 
 /* Private define ------------------------------------------------------------*/
@@ -50,17 +51,31 @@
 /* Private variables ---------------------------------------------------------*/
 
 /* USER CODE BEGIN PV */
+char  rx_buffer [2];
+int inc = 0; // Wypelnienie PWM
+int temporary; // Zmienna tymczasowa
 
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 /* USER CODE BEGIN PFP */
-
+int8_t bmp280_spi_reg_write (uint8_t cs, uint8_t reg_addr, uint8_t *reg_data, uint16_t length);
+int8_t bmp280_spi_reg_read (uint8_t cs, uint8_t reg_addr, uint8_t *reg_data, uint16_t length);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+
+void  HAL_UART_RxCpltCallback(UART_HandleTypeDef  *huart)
+{
+	if (huart->Instance == USART3)
+	{
+		temporary = ((int)rx_buffer [2] - 48) +   ((int)rx_buffer [1] - 48)*10 + ((int)rx_buffer [0] - 48)*100;
+		inc = temporary*10;
+	}
+	HAL_UART_Receive_IT   (&huart3  , (uint8_t*) rx_buffer  , 3); //Zadawanie wypełnienia  PWM do tranzystora [0 3.3V]
+}
 
 /* USER CODE END 0 */
 
@@ -71,7 +86,19 @@ void SystemClock_Config(void);
 int main(void)
 {
   /* USER CODE BEGIN 1 */
+struct bmp280_dev bmp280_1 = {
+		.dev_id = BMP280_CS1,
+		.intf = BMP280_SPI_INTF,
+		.read = bmp280_spi_reg_read, .write = bmp280_spi_reg_write,
+		.delay_ms = HAL_Delay
+};
 
+struct bmp280_uncomp_data bmp280_1_data;
+
+int32_t temp32;
+double temp;
+int32_t pres32;
+double pres;
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
@@ -92,18 +119,47 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  MX_DMA_Init();
   MX_USART3_UART_Init();
   MX_TIM2_Init();
   MX_TIM3_Init();
   MX_TIM4_Init();
   MX_I2C1_Init();
   MX_SPI4_Init();
-  MX_ADC1_Init();
-  MX_DAC_Init();
-  MX_TIM6_Init();
-  MX_TIM7_Init();
   /* USER CODE BEGIN 2 */
+
+  HAL_UART_Receive_IT   (&huart3  , (uint8_t*) rx_buffer  , 3);
+
+
+  HAL_StatusTypeDef tx_status = HAL_OK;
+
+  HAL_TIM_Base_Init(&htim3); // Inicjalizacja timera 3 odpowiedzialnego za PWM
+
+  //Initialize BMP280 sensor
+  int8_t rslt;
+  struct bmp280_config conf;
+
+  rslt = bmp280_init(&bmp280_1);
+  /* Always read the current settings before writing, especially when all the configuration is not modified */
+  rslt = bmp280_get_config(&conf, &bmp280_1);
+  conf.filter = BMP280_FILTER_OFF;
+  /* Temperature oversampling set at 4x */
+  conf.os_temp = BMP280_OS_1X;
+  /* Pressure over sampling none (disabling pressure measurement) */
+  conf.os_pres = BMP280_OS_1X;
+
+  /* Setting the output data rate as 4HZ(250ms) */
+  conf.odr = BMP280_ODR_1000_MS;
+
+  rslt = bmp280_set_config(&conf, &bmp280_1);
+
+  rslt = bmp280_set_power_mode(BMP280_NORMAL_MODE, &bmp280_1);
+
+  char str[4];
+
+ // HAL_TIM_Base_Start(&htim3);
+  //__HAL_TIM_SET_AUTORELOAD  (&htim3  , 999);
+
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_4); // Start PWM
 
   /* USER CODE END 2 */
 
@@ -111,6 +167,29 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+	  __HAL_TIM_SET_COMPARE(&htim3,TIM_CHANNEL_4,inc); // Nadpisanie wartości wypełnienia PWM kanał 4
+	  //Read BMP280 sensor
+      /* Reading the raw data from sensor */
+      rslt = bmp280_get_uncomp_data(&bmp280_1_data, &bmp280_1);
+
+      /* Getting the 32 bit compensated temperature */
+      rslt = bmp280_get_comp_temp_32bit(&temp32, bmp280_1_data.uncomp_temp, &bmp280_1);
+
+      /* Getting the compensated temperature as floating point value */
+      rslt = bmp280_get_comp_temp_double(&temp, bmp280_1_data.uncomp_temp, &bmp280_1);
+      //int itemp = (int)temp;
+      //rslt = bmp280_get_comp_pres_32bit(&pres32, bmp280_1_data.uncomp_press, &bmp280_1);
+
+      /* Getting the compensated temperature as floating point value */
+      //rslt = bmp280_get_comp_pres_double(&pres, bmp280_1_data.uncomp_press, &bmp280_1);
+      //int ipres = (int)pres;
+      /* Sleep time between measurements = BMP280_ODR_250_MS */
+       bmp280_1.delay_ms(1000);
+
+       sprintf(str, "%f", temp);
+
+       tx_status = HAL_UART_Transmit(&huart3,(uint8_t*) str, 5, 100); // Wyslanie temperatury
+       HAL_Delay(1000); //czas odczytu
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -179,6 +258,97 @@ void SystemClock_Config(void)
 }
 
 /* USER CODE BEGIN 4 */
+#define BMP280_SPI_BUFFER_LEN 28
+#define BMP280_DATA_INDEX     1
+
+/*!
+ * @brief Function for writing the sensor 's registers through SPI bus.
+ *
+ * @param [in] cs : Chip select to enable the sensor .
+ * @param [in] reg_addr : Register address .
+ * @param [in] reg_data : Pointer to the data buffer whose data has to be written
+ * @param [in] length : No of bytes to write .
+ *
+ * @return Status of execution
+ * @retval 0 -> Success
+ * @retval >0 -> Failure Info
+ *
+ */
+ int8_t bmp280_spi_reg_write ( uint8_t cs , uint8_t reg_addr , uint8_t * reg_data , uint16_t length )
+ {
+ /* Implement the SPI write routine according to the target machine . */
+ HAL_StatusTypeDef status = HAL_OK ;
+ int32_t iError = BMP280_OK ;
+ uint8_t txarray [ BMP280_SPI_BUFFER_LEN ];
+ uint8_t stringpos ;
+
+ /* Copy register address and data to tx buffer */
+ txarray [0] = reg_addr ;
+ for ( stringpos = 0; stringpos < length ; stringpos ++)
+ {
+ txarray [ stringpos + BMP280_DATA_INDEX ] = reg_data [ stringpos ];
+ }
+ // memcpy ( txarray + BMP280_DATA_INDEX , reg_data , length );
+
+ /* Software slave selection procedure */
+ if( cs == BMP280_CS1 )
+ HAL_GPIO_WritePin ( BMP280_CS1_GPIO_Port , BMP280_CS1_Pin , GPIO_PIN_RESET ) ;
+
+ /* Data exchange */
+ status = HAL_SPI_Transmit ( BMP280_SPI , ( uint8_t *) (& txarray ) , length +1 , 100) ;
+ while ( BMP280_SPI->State == HAL_SPI_STATE_BUSY ) {};
+
+ /* Disable all slaves */
+ HAL_GPIO_WritePin ( BMP280_CS1_GPIO_Port , BMP280_CS1_Pin , GPIO_PIN_SET ) ;
+
+ if ( status != HAL_OK )
+ {
+ // The BMP280 API calls for 0 return value as a success , and -1 returned as failure
+ iError = ( -1) ;
+ }
+ return ( int8_t ) iError ;
+ }
+
+
+ int8_t bmp280_spi_reg_read ( uint8_t cs , uint8_t reg_addr , uint8_t * reg_data , uint16_t length )
+ {
+ /* Implement the SPI read routine according to the target machine . */
+ HAL_StatusTypeDef status = HAL_OK ;
+ int32_t iError = BMP280_OK ;
+ uint8_t txarray [ BMP280_SPI_BUFFER_LEN ] = {0 ,};
+ uint8_t rxarray [ BMP280_SPI_BUFFER_LEN ] = {0 ,};
+ uint8_t stringpos ;
+
+ txarray [0] = reg_addr ;
+
+ /* Software slave selection procedure */
+ if( cs == BMP280_CS1 )
+ HAL_GPIO_WritePin ( BMP280_CS1_GPIO_Port , BMP280_CS1_Pin , GPIO_PIN_RESET ) ;
+
+ /* Data exchange */
+ status = HAL_SPI_TransmitReceive ( BMP280_SPI , ( uint8_t *) (& txarray ) , ( uint8_t *) (&rxarray ) , length +1 , 5) ;
+ while ( BMP280_SPI-> State == HAL_SPI_STATE_BUSY ) {};
+
+ /* Disable all slaves */
+ HAL_GPIO_WritePin ( BMP280_CS1_GPIO_Port , BMP280_CS1_Pin , GPIO_PIN_SET ) ;
+
+ /* Copy data from rx buffer */
+ for ( stringpos = 0; stringpos < length ; stringpos ++)
+ {
+ reg_data [ stringpos ] = rxarray [ stringpos + BMP280_DATA_INDEX ];
+ }
+ // memcpy ( reg_data , rxarray + BMP280_DATA_INDEX , length );
+
+ if ( status != HAL_OK )
+ {
+ // The BME280 API calls for 0 return value as a success , and -1 returned as failure
+ iError = ( -1) ;
+ }
+
+ return ( int8_t ) iError ;
+ }
+
+
 
 /* USER CODE END 4 */
 
@@ -190,10 +360,7 @@ void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
   /* User can add his own implementation to report the HAL error return state */
-  __disable_irq();
-  while (1)
-  {
-  }
+
   /* USER CODE END Error_Handler_Debug */
 }
 
@@ -209,7 +376,7 @@ void assert_failed(uint8_t *file, uint32_t line)
 {
   /* USER CODE BEGIN 6 */
   /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
   /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
