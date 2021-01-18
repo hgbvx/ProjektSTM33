8459166d65ece60ee76534659aae4232a8a4a8ea
hgbvx <hgbvx33@gmail.com>
2021-01-18 21:03:09 +0100

    UART odczyt zadanej temperatury

--- Debug/Core/Src/main.d
+++ Debug/Core/Src/main.d
@@ -35,7 +35,8 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
  ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_tim_ex.h \
  ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_uart.h \
  ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_uart_ex.h \
- ../Core/Inc/i2c.h ../Core/Inc/main.h ../Core/Inc/spi.h ../Core/Inc/tim.h \
+ ../Core/Inc/adc.h ../Core/Inc/main.h ../Core/Inc/dac.h ../Core/Inc/dma.h \
+ ../Core/Inc/i2c.h ../Core/Inc/spi.h ../Core/Inc/tim.h \
  ../Core/Inc/usart.h ../Core/Inc/gpio.h ../Core/Inc/bmp280.h \
  ../Core/Inc/bmp280_defs.h ../Core/Inc/bmp280_defs.h
 
@@ -113,9 +114,15 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
 
 ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_uart_ex.h:
 
-../Core/Inc/i2c.h:
+../Core/Inc/adc.h:
 
 ../Core/Inc/main.h:
+
+../Core/Inc/dac.h:
+
+../Core/Inc/dma.h:
+
+../Core/Inc/i2c.h:
 
 ../Core/Inc/spi.h:
 
